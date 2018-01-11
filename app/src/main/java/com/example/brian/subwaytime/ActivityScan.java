1c015c93bbabe64ab01c0b6dde92773975848d1d
Artur Janik <janika@109-mac-01.bxscience.me>
2018-01-11 11:54:03 -0500

    did some stuff regarding wifimanager

--- app/src/main/java/com/example/brian/subwaytime/ActivityScan.java
+++ app/src/main/java/com/example/brian/subwaytime/ActivityScan.java
@@ -1,5 +1,7 @@
 package com.example.brian.subwaytime;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,9 +10,12 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class ActivityScan extends AppCompatActivity {
-    boolean hasStarted=false;
+    boolean hasStarted = false;
     Button button;
     TextView text;
+
+    WifiManager mainwifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,12 +38,25 @@ public class ActivityScan extends AppCompatActivity {
         if(!hasStarted){
             button.setText("Stop");
             text.setText("Running");
-            hasStarted=true;
+            if (mainwifi.startScan()){
+                Log.d("wifistuff", "wifi successfuly started");
+                hasStarted = true;
+            } else {
+                Log.d("wifistuff", "serious err, couldn't start wifi");//TODO PERMISSIONS
+                hasStarted = false;
+            }
         }
         else if(hasStarted){
             button.setText("Start");
             text.setText("Finish");
             hasStarted=false;
         }
+        //test code here
+
+
+    }
+
+    private String wifiOut(){
+        return mainwifi.getScanResults().toString();
     }
 }
