eda5f6f491bef228fafb8f552fa6ce21a9be11b0
Brian Lee <blee025432@gmail.com>
2018-01-17 11:55:21 -0500

    set mainactivity xml to start

--- app/src/main/java/com/example/brian/subwaytime/StartActivity.java
+++ app/src/main/java/com/example/brian/subwaytime/StartActivity.java
@@ -2,12 +2,39 @@ package com.example.brian.subwaytime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
+    Button activityScan;
+    Button mainActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        activityScan = findViewById(R.id.button2);
+        mainActivity = findViewById(R.id.button3);
+
+
+        //portal to easily switch between apps
+        //main activity to be set here
+
+        activityScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add portal to ActivityScan
+            }
+        });
+
+        mainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add portal to MainActivity
+            }
+        });
     }
+
+
+
 }
