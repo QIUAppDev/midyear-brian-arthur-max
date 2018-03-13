42a77a5e2d085cb9d72d3729b6b62de88b0c2d65
Bronx Science <bxsci@Bronxs-iMac-3.local>
2018-03-13 11:54:38 -0400

    we need to make this work, soon.

---
+++ app/src/main/java/com/example/brian/subwaytime/dataGet.java
@@ -0,0 +1,14 @@
+package com.example.brian.subwaytime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by bxsci on 3/13/18.
+ */
+
+public class dataGet extends AppCompatActivity implements View.OnClickListener{
+
+
+
+}
