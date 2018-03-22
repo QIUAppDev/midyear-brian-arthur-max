656038eeab9aa690f92fe2799650744a4e0b66ae
Brian Lee <blee025432@gmail.com>
2018-03-22 11:46:38 -0400

    added comments for chart

---
+++ app/src/main/java/com/example/brian/subwaytime/TestStore.java
@@ -0,0 +1,15 @@
+package com.example.brian.subwaytime;
+
+/**
+ * Created by brian on 3/22/18.
+ *
+ * a test class to implement proof-of-concepts for all new classes
+ * in anticipation of docker
+ */
+
+public class TestStore {
+
+    //Store<ArticleAsset, Integer> store;
+
+    public void storeInit(){}
+}
--- app/src/main/java/com/example/brian/subwaytime/dataGet.java
+++ app/src/main/java/com/example/brian/subwaytime/dataGet.java
@@ -8,7 +8,7 @@ import android.view.View;
  */
 
 public class dataGet extends AppCompatActivity implements View.OnClickListener{
-
+    public void onClick(View view){} //temporary method to enable debug runs to pass
 
 
 }
