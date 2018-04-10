c50c478792ccb9f4fbbd33af377520eeffb92373
Brian Lee <leebr1@bxscience.edu>
2018-04-10 04:31:52 +0000

    pushing works with room

--- app/src/main/java/com/example/brian/subwaytime/FirebaseTest.java
+++ app/src/main/java/com/example/brian/subwaytime/FirebaseTest.java
@@ -1,8 +1,11 @@
 package com.example.brian.subwaytime;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -14,11 +17,20 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class FirebaseTest extends AppCompatActivity {
 
-    FirebaseDatabase database;
-    DatabaseReference myRef;
+    //FIREBASE stuff
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
+    //Room Persistence DB
+    final AppDatabase appDatabase = AppDatabase.getDatabase(this);
+
+    private Button push_to_firebase;
+    private Button pull_from_firebase;
+    private Button back_to_menu;
 
     private static final String TAG = "aaaaaaaaaa";
 
@@ -27,11 +39,41 @@ public class FirebaseTest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_test);
 
+        //init the database
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
 
+        //init the buttons and the UI
+        push_to_firebase = findViewById(R.id.button4);
+        pull_from_firebase = findViewById(R.id.button5);
+        back_to_menu = findViewById(R.id.button6);
+
+        push_to_firebase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pushFirebase();
+            }
+        });
+
+        pull_from_firebase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pullFirebase();
+            }
+        });
+
+        back_to_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: add back to menu
+            }
+        });
+
+    }
+
+    public void pullFirebase(){
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -55,18 +97,18 @@ public class FirebaseTest extends AppCompatActivity {
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
-
-        pushNewData();
     }
 
-    public void pushNewData(){ //uses the derpwork class STRUCTURE to add to firebase
+    public void pushFirebase(){ //uses the derpwork class STRUCTURE to add to firebase
         //proposed feature: since our DB is literally just one table, a simple method can read off incoming JSON files and write into db
         //advantages: boilerplate recyclerview code remains untouched
 
-        Log.d("testData","progress got here");
-
         //generates a test object and appends to the wifi network
         //ideally, on the subway run, Room persistence captures the wifi networks, and Firebase bundles them up and pushes them
+
+        Log.d("testData","progress got here");
+
+        /*
         derpwork test_network = new derpwork();
         String MAC="mac_1";
         test_network.setName("station_1"); //again, to store the actual station name
@@ -78,10 +120,26 @@ public class FirebaseTest extends AppCompatActivity {
         test_network.setMac(MAC);
         test_network.setPasspoint("passpoint_1");
         test_network.setTimestamp("timestamp 1");
+        */
 
-        HashMap<String, derpwork> wifi_push = new HashMap<>();
-        wifi_push.put("station_1",test_network); //station_1 corresponds to the key and will be replaced by something better as a key
-        myRef.setValue(wifi_push);
+
+        //pulls the networks accumulated in the local Room database, and pushes it onto Firebase
+        new AsyncTask<Void,Void,Void>(){
+            protected Void doInBackground(Void... params){
+                List<derpwork> all_networks = appDatabase.networkDao().getAll_nonLiveData();
+                HashMap<String, derpwork> wifi_push = new HashMap<>();
+                for(derpwork network : all_networks){
+                    wifi_push.put(network.getSsid(), network);
+                }
+                myRef.setValue(wifi_push);
+
+                return null;
+            }
+        }.execute();
+
+
+        //wifi_push.put("station_1",test_network);
+
         //myRef.child("station_1").setValue(test_network);
 
 
