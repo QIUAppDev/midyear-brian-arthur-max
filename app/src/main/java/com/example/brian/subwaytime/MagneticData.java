b6f0b68de44a06e46130a260d7ca0aa33f975d1f
Brian Lee <leebr1@bxscience.edu>
2018-04-14 01:14:27 +0000

    started firebase push

--- app/src/main/java/com/example/brian/subwaytime/MagneticData.java
+++ app/src/main/java/com/example/brian/subwaytime/MagneticData.java
@@ -17,8 +17,12 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static android.hardware.Sensor.TYPE_MAGNETIC_FIELD;
 
@@ -26,13 +30,19 @@ public class MagneticData extends AppCompatActivity implements SensorEventListen
 
     //magnetic data (and corresponding) timestamps that get posted by user ID to firebase
     //pushes to firebase every 10 seconds (i.e. when the unix timestamp is divisible by 10)
-    public ArrayList<float[]> TODOBRIANFIXTHIS = new ArrayList<>();
-    public ArrayList<Long> timestamps = new ArrayList<>();
+    public List<float[]> TODOBRIANFIXTHIS = new ArrayList();
+    public List<Long> timestamps = new ArrayList();
 
     //magnetic stuff
     private SensorManager mSensorManager;
     private Sensor mSensor;
     private String TAG = "MagneticData";
+
+    //FIREBASE stuff
+    //a different point of reference will be accessed than FirebaseTest to post users
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
 
 //    https://developer.android.com/guide/topics/sensors/sensors_motion.html#sensors-motion-accel
     @Override
@@ -90,6 +100,11 @@ public class MagneticData extends AppCompatActivity implements SensorEventListen
 
             }
         });
+
+        //init the firebase
+        //init the database
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("users");
     }
     protected void onResume(){
         super.onResume();
@@ -108,6 +123,10 @@ public class MagneticData extends AppCompatActivity implements SensorEventListen
         //TODO: INTEGRATE WIFI AND MAGNETISM
         if(timestamps.size()>1){
             if(!timestamps.get(timestamps.size()-1).equals(timestamps.get(timestamps.size()-2)) && timestamps.get(timestamps.size()-1)%10==0){
+                String phone_id = PersistentID.get_id();
+                myRef.child("users").child(phone_id).child("persistent_id").setValue(phone_id);
+                myRef.child("users").child(phone_id).child("magnetic_data").setValue(TODOBRIANFIXTHIS);
+                myRef.child("users").child(phone_id).child("timestamps").setValue(timestamps);
                 Log.d("push_to_firebase","10 seconds");
             }
         }
