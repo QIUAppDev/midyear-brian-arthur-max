2dd046e3ecb54679c512c4f95ffc97de7f62ef96
Brian Lee <leebr1@bxscience.edu>
2018-04-15 03:59:48 +0000

    created new folder for new system

---
+++ app/src/main/java/com/example/brian/subwaytime/UnifiedSystem/UnifedMain.java
@@ -0,0 +1,15 @@
+package com.example.brian.subwaytime.UnifiedSystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.brian.subwaytime.R;
+
+public class UnifedMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unifed_main);
+    }
+}
